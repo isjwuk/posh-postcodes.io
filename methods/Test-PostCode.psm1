07d2d9db3212edfecf6a6ea0c34e4378f555d60e
isjwuk <{ID}+{username}@users.noreply.github.com>
2024-07-11 18:44:21 +0100

    working implementation

--- methods/Test-PostCode.psm1
+++ methods/Test-PostCode.psm1
@@ -1,25 +1,49 @@
-#TODO Complete this function
+$API_ROOT="https://api.postcodes.io"
+
 <#
 .SYNOPSIS
     Convenience method to validate a postcode. 
 .DESCRIPTION
-    Returns true if postcode is valid, otherwise false
+    Returns true if the given postcode is valid, otherwise false
 .NOTES
-    Information or caveats about the function e.g. 'This function is not supported in Linux'
+    
 .LINK
-    Specify a URI to a help page, this will show when Get-Help -Online is used.
+
 .EXAMPLE
-    Test-MyTestFunction -Verbose
-    Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+    Test-PostCode -Postcode "EH99 1SP"
+    (or    Test-PostCode -Postcode "EH991sp"  )
+    Validate the Postcode EH99 1SP. This returns true for both as the postcode is valid and the function is case and space insensitive. 
+.EXAMPLE
+    Test-PostCode -Postcode "Banana"
+    Returns False as this postcode is very, very invalid.
+.EXAMPLE
+    "GU1 1AA", "GU1 1AB", "GU1 1AC", "GU1 1AD"  | Test-PostCode
+    Validates each of these four correct-looking postcodes. The result is
+        True
+        False
+        False
+        True
+    As the first and fourth postcodes are valid and the middle two, despite being the correct format, don't exist in the database.
 #>
-Boolean function Test-PostCode {
+function Test-PostCode {
     [CmdletBinding()]
+    [OutputType([Boolean])]
     param(
-  #    [parameter(ValueFromPipeline)]
+        #Postcode to Validate. Required string. Case and space insensitive, examples 'SW1A 1AA', 'CF991SN'
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true)]
+            [string] $Postcode
     )
   
     process {
-      
+        $result=Invoke-RestMethod -Uri "$API_ROOT/postcodes/$Postcode/validate" -Method Get -SkipHttpErrorCheck
+        switch ($result.status) {
+            200 {
+               $result.result 
+              }
+            Default {
+                throw $result.error
+            }
+        }
     }
   }
   
