5ac83fefba715653d10cd635f072aaad5931c5f1
isjwuk <{ID}+{username}@users.noreply.github.com>
2024-07-12 16:07:58 +0100

    Working with tests

--- methods/Get-NearestPostcode.psm1
+++ methods/Get-NearestPostcode.psm1
@@ -4,14 +4,27 @@ $API_ROOT="https://api.postcodes.io"
 .SYNOPSIS
     Returns nearest postcodes for a given longitude and latitude, or a given postcode.
 .DESCRIPTION
-    A longer description of the function, its purpose, common use cases, etc.
+    Returns nearest postcodes for a given longitude and latitude, or a given postcode.
 .NOTES
-    Information or caveats about the function e.g. 'This function is not supported in Linux'
+    
 .LINK
-    Specify a URI to a help page, this will show when Get-Help -Online is used.
+    
+.EXAMPLE
+    Get-NearestPostcode -Latitude 51.5012 -Longitude -0.1419
+    Return all the data associated with the postcode for the selected Latitude and Longitude.
 .EXAMPLE
     (Get-NearestPostcode -Latitude 51.5012 -Longitude -0.1419).Postcode
-    Return the postcode for the selected Latitude and Longitude
+    Return only the postcode for the selected Latitude and Longitude.
+.EXAMPLE
+    Get-NearestPostcode -Latitude 51.406207 -Longitude -0.337634 | Select Postcode, distance
+    Return multiple nearby postcodes in the event of a non-postcoded location.
+.EXAMPLE
+    Get-NearestPostcode -Latitude 51.406207 -Longitude -0.337634 -limit 1
+    Return the closest nearby postcode in the event of a non-postcoded location.
+.EXAMPLE   
+    Get-NearestPostcode -Latitude 51.506645 -Longitude 0.003015 -radius 250
+    Use the radius parameter to look further afield for the nearest postcode.
+
 #>
 function Get-NearestPostcode  {
   [CmdletBinding()]
@@ -21,16 +34,31 @@ function Get-NearestPostcode  {
     [float]$Latitude,
     #Latitude pf Geolocation to return postcodes for
     [Parameter(Mandatory, ParameterSetName="GeoLocation")]
-    [float]$Longitude
+    [float]$Longitude,
+    #(not required) Limits number of postcodes matches to return. Defaults to 10. Needs to be less than 100.
+    [Parameter(Mandatory=$false, ParameterSetName="GeoLocation")]
+    [ValidateRange(1,99)]
+    [int]$limit=10,
+    #(not required) Limits number of postcodes matches to return. Defaults to 100m. Needs to be less than 2,000m.
+    [Parameter(Mandatory=$false, ParameterSetName="GeoLocation")]
+    [ValidateRange(1,1999)]
+    [int]$radius=100,
+    #(not required) Search up to 20km radius, but subject to a maximum of 10 results. Since lookups over a wide area can be very expensive, we've created this method to allow you choose to make the trade off between search radius and number of results. Defaults to false. When enabled, radius and limits over 10 are ignored.
+    [Parameter(Mandatory=$false, ParameterSetName="GeoLocation")]
+    [switch]$widesearch
   )
 
   process {
     #Decide which API to use based on the Parameter set used when calling this function
     switch ($PSCmdlet.ParameterSetName) {
         'GeoLocation' {
-            $result=Invoke-RestMethod -Uri "$API_ROOT/postcodes/?lon=$longitude&lat=$latitude" -Method Get -SkipHttpErrorCheck
+            $URI="$API_ROOT/postcodes/?lon=$longitude&lat=$latitude&limit=$limit&radius=$radius"
+            if ($widesearch ){$URI+="&widesearch=true"} # Add the Widesearch parameter if selected.
+            $result=Invoke-RestMethod -Uri $URI -Method Get -SkipHttpErrorCheck
         }
     }
+
+    #Return the results
     switch ($result.status) {
         200 {
             $result.result 
--- tests/Get-NearestPostcode.Tests.ps1
+++ tests/Get-NearestPostcode.Tests.ps1
@@ -7,16 +7,18 @@ Describe 'Get-NearestPostcode' {
         $result=Get-NearestPostcode -Latitude 51.5012 -Longitude -0.1419
         $result.Postcode | Should -Be 'SW1A 1AA'
     }
-    # It 'Given a nonsense postcode it returns an Invalid Postcode error' {
-    #     { Get-PostCodeInfo -Postcode 'Banana'} | Should -Throw -ExpectedMessage "Invalid postcode"
-    # }
-    # It 'Given two valid postcodes on the pipeline, it returns values for both' {
-    #     $result='EH99 1SP','SW1A 1AA' | Get-PostCodeInfo
-    #     $result[0].country + $result[1].country | Should -Be 'ScotlandEngland'
-    # }
-    # It 'Given a valid postcode with mixed case and no space it returns data'{
-    #     $result=Get-PostCodeInfo -Postcode "Cf991sn"
-    #     $result.Country | Should -Be 'Wales'
-    # }
-
+    It 'Given a non-postcoded location return multiple postcodes' {
+        $result=Get-NearestPostcode -Latitude 51.406207 -Longitude -0.337634
+        $result.count | Should -Be 4
+    }
+    It 'Given a non-postcoded location return multiple postcodes but use the limit parameter' {
+        $result=Get-NearestPostcode -Latitude 51.406207 -Longitude -0.337634 -limit 2
+        $result.count | Should -Be 2
+    }
+    It 'Given a non-postcoded location return the postcodes using the range parameter' {
+        $result=Get-NearestPostcode -Latitude 51.506645 -Longitude 0.003015 
+        $result | Should -Be $null
+        $result=Get-NearestPostcode -Latitude 51.506645 -Longitude 0.003015 -radius 250
+        $result.count | Should -Be 2
+    }
 }
