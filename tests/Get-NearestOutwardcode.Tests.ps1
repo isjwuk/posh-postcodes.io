6c8c8d4f84c16ba6cd3c3e53fad29166ca0d5f2e
isjwuk <{ID}+{username}@users.noreply.github.com>
2024-07-18 10:43:08 +0100

    implemented get-nearestoutwardcode with tests

---
+++ tests/Get-NearestOutwardcode.Tests.ps1
@@ -0,0 +1,17 @@
+# Pester Tests for Get-NearestPostcode
+BeforeAll {
+    Import-Module $PSScriptRoot/../Posh-Postcodes.io.psd1
+}
+Describe 'Get-NearestOutwardcode' {
+    It 'Given a valid geolocation it returns postcode data' {
+        $result=Get-NearestOutwardCode -Latitude 51.50460 -Longitude -0.13217 -limit 1
+        $result.Outcode | Should -Be 'SW1A'
+    }
+    It 'Given a non-postcoded location return multiple postcodes' {
+        $result=Get-NearestOutwardCode -outcode "EC4M" | Select-Object outcode
+        $result.count | Should -Be 10
+    }
+    It 'Given a non-postcoded location return the postcodes using the range parameter' {
+         { Get-NearestOutCode -outcode "banana"} | Should -Throw -ExpectedMessage "Outcode not found"
+    }
+}
