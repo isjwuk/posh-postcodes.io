dff9078abdf019610de3ee76ce4d0b9fd5d5b4c2
isjwuk <{ID}+{username}@users.noreply.github.com>
2024-07-18 18:38:49 +0100

    included examples in help comment

--- methods/Get-OutwardCode.psm1
+++ methods/Get-OutwardCode.psm1
@@ -5,8 +5,11 @@ $API_ROOT="https://api.postcodes.io"
 .DESCRIPTION
     Geolocation (latitude and longitude) data for the centroid of the outward code specified. The outward code represents the first half of any postcode (separated by a space).
 .EXAMPLE
-    Test-MyTestFunction -Verbose #TODO Examples
-    Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+    Get-OutwardCode "GU1"
+    Return the full geolocation data for the Outward code "GU1"
+.EXAMPLE
+    Get-OutwardCode "SW19" | Select-Object Latitude, Longitude
+    Get the latitude and longitude for the centroid of the Outcode SW19.
 #>
 function Get-OutwardCode {
     [alias("Get-OutCode")]
